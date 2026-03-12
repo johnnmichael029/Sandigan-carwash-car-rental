a4bc33c501ce68faafdd9d283e4e55582ad3a5b9
John Michael De Torres <86936567+johnnmichael029@users.noreply.github.com>
2026-03-12 08:34:14 +0800

    Add project files.

---
+++ ExpressProject1/index.js
@@ -0,0 +1,27 @@
+﻿const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+
+// Connect to MongoDB (SandiganDB will be created automatically)
+mongoose.connect('mongodb://localhost:27017/SandiganDB')
+    .then(() => console.log('✅ Success: Connected to MongoDB!'))
+    .catch(err => console.error('❌ Database connection error:', err));
+
+app.set('view engine', 'ejs'); // Tell Express to use EJS
+app.use(express.urlencoded({ extended: true })); // This helps read form data
+
+app.get('/', (req, res) => {
+    res.render('client/index'); // This looks for 'views/index.ejs'
+});
+
+// This handles the form submission
+app.post('/book', (req, res) => {
+    const data = req.body;
+    console.log("New Booking Received:", data);
+    res.send(`Successfully received booking for ${data.customerName}! Check your terminal.`);
+});
+
+app.listen(port, () => {
+    console.log(`✅ Server live at http://localhost:${port}`);
+});
